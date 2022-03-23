67de3619c88a5ef9565a7de57c63b95e23798a51
Aaryan-Thakur <aaryan3120@gmail.com>
2022-03-23 16:49:31 +0530

    added category section to posts

--- src/main/java/com/example/demo/Post.java
+++ src/main/java/com/example/demo/Post.java
@@ -13,81 +13,128 @@ import javax.persistence.Table;
 public class Post {
 
 	
-	
+///////////////////////////////////////////////////////////////////
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
-	
-	@Column(nullable = true,length=100)	
-	public String Title;
-	
-	@Column(nullable = true,length=500)	
-	public String Description;
-	
-	@Column(nullable = true,unique=false,length=45)	
-	public String username;
-	
-	@Column(nullable = true,length=500)	
-	public String Respone;
-	
-	@Column(nullable = true,length=10)	
-	public LocalDate Create_Date;
-	
-	@Column(nullable = true,length=100)	
-	public String Email;
-	
-	@Column(nullable = true,unique=false,length=45)	
-	public String Responder;
-	
-	public Long getOP_id() {
-		return OP_id;
-	}
-	public void setOP_id(Long oP_id) {
-		OP_id = oP_id;
-	}
-	@Column(nullable = true)	
-	public Long OP_id;
-	
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String email) {
-		Email = email;
-	}
-	public String getRespone() {
-		return Respone;
-	}
-	public void setRespone(String respone) {
-		Respone = respone;
-	}
-	public LocalDate getCreate_Date() {
-		return Create_Date;
-	}
-	public void setCreate_Date(LocalDate create_Date) {
-		Create_Date = create_Date;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+/////////////////////////////////////////////////////////////////////
+
+	@Column(nullable = true)	
+	public Long OP_id;
+
+	public Long getOP_id() {
+		return OP_id;
+	}
+	public void setOP_id(Long oP_id) {
+		OP_id = oP_id;
+	}
+	
+///////////////////////////////////////////////////////////////////
+
+	@Column(nullable = true,length=100)	
+	public String Title;
+
 	public String getTitle() {
 		return Title;
 	}
 	public void setTitle(String title) {
 		Title = title;
 	}
+
+///////////////////////////////////////////////////////////////////
+
+	@Column(nullable = true,length=500)	
+	public String Description;
+
 	public String getDescription() {
 		return Description;
 	}
 	public void setDescription(String description) {
 		Description = description;
 	}
+
+////////////////////////////////////////////////////////////////////
+	
+	@Column(nullable = true,unique=false,length=45)	
+	public String username;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+/////////////////////////////////////////////////////////////////////
+
+	@Column(nullable = true,length=500)	
+	public String Respone;
+
+	public String getRespone() {
+		return Respone;
+	}
+	public void setRespone(String respone) {
+		Respone = respone;
+	}
+
+//////////////////////////////////////////////////////////////////////
+
+	@Column(nullable = true,length=10)	
+	public LocalDate Create_Date;
+
+	public LocalDate getCreate_Date() {
+		return Create_Date;
+	}
+	public void setCreate_Date(LocalDate create_Date) {
+		Create_Date = create_Date;
+	}
+
+//////////////////////////////////////////////////////////////////////
+
+	@Column(nullable = true,length=100)	
+	public String Email;
+
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+//////////////////////////////////////////////////////////////////////
+
+
+	@Column(nullable = true,unique=false,length=45)	
+	public String Responder;
+	
+	public String getResponder() {
+		return this.Responder;
+	}
+
+	public void setResponder(String Responder) {
+		this.Responder = Responder;
+	}
+
+/////////////////////////////////////////////////////////////////////
+	public String getCategory() {
+		return this.Category;
+	}
+
+	public void setCategory(String Category) {
+		this.Category = Category;
+	}
+
+	@Column(nullable = true,unique = false)
+	public String Category;
+
+/////////////////////////////////////////////////////////////////////
+
 }
